7474e2a19defe177c9552a26d4ad64a9f78475ad
NerfedJabolo <ezdlprofit1@gmail.com>
2024-08-27 05:16:03 +0300

    navbar and hero title

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,41 @@
-import { useState } from 'react'
+import Layout from './components/Layout/layout';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1 className='text-red-500 font-bold'>HELLO WORLD</h1>
+      <Layout>
+        <nav className="w-full bg-[#FBFBFB] rounded-xl h-16 p-1 shadow-[inset_0px_0px_20px_#F0F0F0] flex justify-between items-center px-6 font-[inter]">
+          <span className="space-x-[13px] flex items-center font-semibold">
+            <img src="./ph_shower-bold.png" alt="masked logo" />
+            <h1 className="text-[26px]">Masked</h1>
+          </span>
+          <ul className="text-[22px] flex lg:gap-16 gap-6">
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="#projects">Projects</a>
+            </li>
+            <li>
+              <a href="/blog">Blog</a>
+            </li>
+          </ul>
+          <button className="rounded-full py-1 px-6 text-[#00982a] text-[22px] bg-[rgb(0,187,52,0.12)]">
+            Contact
+          </button>
+        </nav>
+        <div className="pt-40 text-center">
+          <h1 className="font-serif text-[105px]">
+            Get Your Website{' '}
+            <span className="block">
+              Built by{' '}
+              <span className="text-[#00982a] text-shadow">Experts</span>
+            </span>
+          </h1>
+        </div>
+      </Layout>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
