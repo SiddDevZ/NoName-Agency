4586f5ec4f8e8fcf934dd6d8d7e6ce8d48ca4896
SiddDevZ <epic.siddarth@gmail.com>
2024-08-29 19:24:33 +0530

    Added Background

--- src/components/UI/Navbar.jsx
+++ src/components/UI/Navbar.jsx
@@ -2,22 +2,18 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <nav className="w-full bg-[#FBFBFB] rounded-xl h-16 p-1 shadow-[inset_0px_0px_20px_#F0F0F0] flex justify-between items-center px-6 font-[inter]">
+    <nav className="w-full bg-[#FBFBFB] rounded-xl h-14 p-1 shadow-[inset_0px_0px_20px_#F0F0F0] flex justify-between items-center px-6 font-[inter]">
       <span className="space-x-[13px] flex items-center font-semibold">
-        <img src="./ph_shower-bold.png" alt="masked logo" />
-        <h1 className="text-[26px]">Masked</h1>
+        <img src="./ph_shower-bold.png" alt="masked logo" className='w-[1.9rem]' />
+        <h1 className="text-2xl">Masked</h1>
       </span>
-      <ul className="text-[22px] flex lg:gap-16 gap-6">
-        <li>
-          <a href="/">Home</a>
-        </li>
-        <li>
-          <a href="#projects">Projects</a>
-        </li>
-        <li>
-          <a href="/blog">Blog</a>
-        </li>
-      </ul>
+
+      <div className="text-[21px] flex lg:gap-16 gap-6">
+        <a href="/">Home</a>
+        <a href="#projects">Projects</a>
+        <a href="/blog">Blog</a>
+      </div>
+
       <button className="rounded-full py-1 px-6 text-[#00982a] text-[22px] bg-[rgb(0,187,52,0.12)]">
         Contact
       </button>
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -6,18 +6,24 @@ import Navbar from '.././components/UI/Navbar';
 const Landing = () => {
   return (
     <>
-      <Layout>
-        <Navbar />
-        <div className="pt-40 text-center">
-          <h1 className="font-serif text-[105px]">
-            Get Your Website{' '}
-            <span className="block">
-              Built by{' '}
-              <span className="text-[#00982a] text-shadow">Experts</span>
-            </span>
-          </h1>
-        </div>
-      </Layout>
+      <div className="h-[50rem] w-full  bg-white  bg-grid-black/[0.2] relative flex items-center justify-center">
+        {/* Radial gradient for the container to give a faded look */}
+        <div className="absolute pointer-events-none inset-0 flex items-center justify-center  bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+        <main className="w-[90vw] md:w-[80vw] max-w-[1920px] mx-auto">
+            <Navbar />
+            <div className="pt-[7.6rem] text-center">
+              <h1 className="font-serif text-[5.5rem] leading-[1.15]">
+                Get Your Website
+                <span className="block mt-0">
+                  Built by{' '}
+                  <span className="text-[#00982a] relative">
+                    Experts
+                  </span>
+                </span>
+              </h1>
+            </div>
+        </main>
+      </div>
     </>
   )
 }
