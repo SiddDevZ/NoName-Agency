7b2f24e778b6831274faf2adb2e7fa557aa5fe7b
SiddDevZ <epic.siddarth@gmail.com>
2024-10-09 20:53:36 +0530

    stuff

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,11 +5,11 @@ const Projects = ({ reff }) => {
   const [isHovered, setIsHovered] = useState(false);
 
   const items = [
-    { image: 'project_1.png', description: 'Beautiful landscape' },
-    { image: 'project_2.png', description: 'City skyline' },
-    { image: 'project_3.png', description: 'Mountain view' },
-    { image: 'project_4.png', description: 'Serene beach' },
-    { image: 'project_5.png', description: 'Forest trail' },
+    { image: 'project_1.png', description: 'Our Agency Website' },
+    { image: 'project_2.png', description: 'Portfolio Website' },
+    { image: 'project_5.png', description: 'ZenosAI' },
+    { image: 'project_3.png', description: 'Bantr - Chatting App' },
+    { image: 'project_4.png', description: '53WEST53' },
   ];
 
   return (
